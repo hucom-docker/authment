f7e0052924ef3713c20225b873f200b8236b9024
mingxing47 <398626384@qq.com>
2017-09-19 20:41:41 +0800

    ini

---
+++ src/main/java/com/ming/accessmanagement/AccessmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.ming.accessmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessmanagementApplication.class, args);
+	}
+}
