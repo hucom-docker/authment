0f248ae8b2a1b099c72180c0382012ba56e1fe7b
mingxing47 <398626384@qq.com>
2017-09-19 20:46:58 +0800

    update

--- src/main/java/com/ming/accessmanagement/AccessmanagementApplication.java
+++ src/main/java/com/ming/accessmanagement/AccessmanagementApplication.java
@@ -2,9 +2,18 @@ package com.ming.accessmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AccessmanagementApplication {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessmanagementApplication.class, args);
